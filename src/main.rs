254c8e56d7b138924a21ea898570fc1efbecea0e
Ahmed Zein <ahmedzein3652@gmail.com>
2024-07-15 02:54:57 +0300

    refactor to improve modularity

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,23 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     // the first arg is the program name
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("searching for {query}, in {file_path}");
+    let config = Config::new(&args);
+    println!("searching for {}, in {}", config.query, config.file_path);
 
-    println!("In File {file_path}");
-    let contents = fs::read_to_string(file_path).expect("Should have been able to open the file");
+    println!("In File {}", config.file_path);
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to open the file");
     println!("with content\n{contents}");
-    
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
