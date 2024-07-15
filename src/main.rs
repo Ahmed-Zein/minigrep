776bbc1178151df6122ab785d4457ae7ee82ce8b
Ahmed Zein <ahmedzein3652@gmail.com>
2024-07-15 16:55:10 +0300

    add::search fun

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+
     println!("searching for {}, in {}", config.query, config.file_path);
+
     if let Err(e) = minigrep::run(config) {
         println!("Application Error: {e}");
         process::exit(1);
