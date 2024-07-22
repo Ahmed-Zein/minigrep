0ec4570724abf30cc8f08c5da74f5a891a4b0fc6
Ahmed Zein <ahmedzein3652@gmail.com>
2024-07-23 02:25:09 +0300

    use iters

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,8 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // dbg!(args);
-    // the first arg is the program name
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
